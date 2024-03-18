ef138242f956149d4e51dfdc53bf4d2cfd47bfcb
HeinzLoutveheBlack <prathamcsgupta@gmail.com>
2024-03-19 00:07:21 +0530

    Testfilter updated, boxing the corners

--- Testfilter.py
+++ Testfilter.py
@@ -10,22 +10,14 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def find_centroids(dst):
-    ret, dst = cv2.threshold(dst, 0.01 * dst.max(), 255, 0)
-    dst = np.uint8(dst)
 
-    # find centroids
-    ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
-    # define the criteria to stop and refine the corners
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 
-                0.001)
-    corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5), 
-              (-1,-1),criteria)
-    return corners
+load = cv2.imread('testImages/Q5OG.jpg')
+loadcopy = cv2.imread('testImages/Q5OG.jpg')
 
+'''load = cv2.imread('testImages/cropped.png')
+loadcopy = cv2.imread('testImages/cropped.png')'''
 
-image = cv2.imread('Q5OG.jpg')
-imagetbg = cv2.imread('Q5OG.png')
+'''imagetbg = cv2.imread('Q5OG.png')
 down_width = 256
 down_height = 256
 down_points = (down_width, down_height)
@@ -38,14 +30,51 @@ res= cv2.hconcat([resized_down,resultT,result])
 cv2.imshow('Res', res)
 cv2.waitKey()
 
-gray = cv2.cvtColor(imagetbg, cv2.COLOR_BGR2GRAY)
-cv2.imshow('Grayed32', gray)
+graysc = cv2.cvtColor(imagetbg, cv2.COLOR_BGR2GRAY)
+cv2.imshow('Grayed32', graysc)
+cv2.waitKey()
+ '''
+down_width = 720
+down_height = 720
+down_points = (down_width, down_height)
+image = cv2.resize(load, down_points, interpolation= cv2.INTER_LINEAR)
+imagecopy = cv2.resize(loadcopy, down_points, interpolation= cv2.INTER_LINEAR)
+
+gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+gray = np.float32(gray)
+dst = cv2.cornerHarris(gray,2,3,0.04)
+ 
+#result is dilated for marking the corners, not important
+dst = cv2.dilate(dst,None)
+ 
+# Threshold for an optimal value, it may vary depending on the image.
+image[dst>0.01*dst.max()]=[255,255,255]
+ 
+'''cv2.imshow('dst',image)
+cv2.waitKey()'''
+#Till here, the code marks all the corners with a white 4 point box.
+
+offset = image-imagecopy 
+offsetgsc=cv2.cvtColor(offset,cv2.COLOR_BGR2GRAY)
+
+#Here, the code finds the difference between the image and the corner added image, so it gives the corners only
+nz=cv2.findNonZero(offsetgsc)
+a = nz[:,0,0].min()
+b = nz[:,0,1].min()
+c = nz[:,0,0].max()
+d = nz[:,0,1].max()
+
+print(a,b)
+print(c,d)
+print(a,d)
+print(c,b)
+
+offset[b,a:c] =120
+offset[b:d,a] =120
+offset[b:d,c] =120
+offset[d,a:c] =120
+
+cv2.imshow('Offset', offset)
 cv2.waitKey()
 
-dst = cv2.cornerHarris(gray, 2, 3, 0.04)
-
-# Threshold for an optimal value, it may vary depending on the image.
-corners= find_centroids(dst)
-# To draw the corners
-
-plt.plot(corners)
