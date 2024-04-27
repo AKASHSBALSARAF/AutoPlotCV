24c8d14debc9ec4909d4917d1ac96dc1a792a057
airwarriorg91 <gauravxpgupta@gmail.com>
2024-04-28 00:55:12 +0530

    ocr-test

--- lengthscalesAndEdges.py
+++ lengthscalesAndEdges.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sun Mar 10 10:52:22 2024
 
 @author: Pratham Gupta
 """
-#This is for testing of filters and basic image processing
+
 
 import cv2 
 import numpy as np
@@ -13,7 +12,7 @@ def lengthscalesAndEdge(load):
     loadcopy = load
 
     down_width = 720
-    down_height = 720
+    down_height = int(loadcopy.shape[0]/loadcopy.shape[1] * 720)
     down_points = (down_width, down_height)
     image = cv2.resize(load, down_points, interpolation= cv2.INTER_LINEAR)
     imagecopy = cv2.resize(loadcopy, down_points, interpolation= cv2.INTER_LINEAR)
@@ -25,48 +24,43 @@ def lengthscalesAndEdge(load):
     image[dst>0.012*dst.max()]=[255,255,255]
 
     offset = cv2.cvtColor(image-imagecopy, cv2.COLOR_BGR2GRAY)
-    '''cv2.imshow('Offset',offset)
-    cv2.waitKey()'''
 
     column_sum=[]
-    for i in range(down_height):
-     column_index = i
-     column_sum.append(np.sum(offset[:, column_index]))
-     i+=1
-    #print(column_sum)
-    #print(max(column_sum))
+    for i in range(down_width):
+        column_index = i
+        column_sum.append(np.sum(offset[:, column_index]))
+
     lvline_index =column_sum.index(max(column_sum))
 
     loopline2_index=lvline_index
     while abs(loopline2_index-lvline_index)<10:
-     column_sum.pop(loopline2_index)
-     loopline2_index = column_sum.index(max(column_sum))
+        column_sum.pop(loopline2_index)
+        loopline2_index = column_sum.index(max(column_sum))
     columntestline_index = loopline2_index
 
     if columntestline_index < lvline_index:
-     vline_index = columntestline_index
+        vline_index = columntestline_index
     else:
-     vline_index=columntestline_index+1
+        vline_index=columntestline_index+1
 
     row_sum=[]
-    for i in range(down_width):
-     row_index = i
-     row_sum.append(np.sum(offset[row_index,:]))
-     i+=1
-    #print(row_sum)
-    #print(max(row_sum))
+    for i in range(down_height):
+        row_index = i
+        row_sum.append(np.sum(offset[row_index,:]))
+
+
     bhline_index =row_sum.index(max(row_sum))
 
     loopline_index=bhline_index
     while abs(loopline_index-bhline_index)<10:
-     row_sum.pop(loopline_index)
-     loopline_index = row_sum.index(max(row_sum))
+        row_sum.pop(loopline_index)
+        loopline_index = row_sum.index(max(row_sum))
     rowtestline_index = loopline_index
 
     if rowtestline_index < bhline_index:
-     hline_index = rowtestline_index
+        hline_index = rowtestline_index
     else:
-     hline_index=rowtestline_index+1
+        hline_index=rowtestline_index+1
 
 
     a = lvline_index
@@ -74,119 +68,92 @@ def lengthscalesAndEdge(load):
     c = vline_index #nz[:,0,0].max()
     d = bhline_index
 
-    #print(b,d,c,a)
 
     offset[b,a:c] =128
     offset[b:d,a] =128
     offset[b:d,c] =128
     offset[d,a:c] =128
-    # cv2.imshow('Offset', offset)
-    # cv2.waitKey()
 
     imagecopy[b,a:c] =(0,255,0)
     imagecopy[d,a:c] =(0,255,0)
     imagecopy[b:d,a] =(0,255,0)
     imagecopy[b:d,c] =(0,255,0)
 
-    # cv2.imshow('Image',imagecopy)
-    # cv2.waitKey()
-
     graph = imagecopy[b-3:d+3,a-3:c+3]
     nongraph = imagecopy
     nongraph[b:d,a:c] = 255
     graphcopy = graph
-
-    # Display cropped image
-    # cv2.imshow("Cropped", graph)
-    # cv2.waitKey()
-
-    # Save the cropped image
-    #cv2.imwrite("testImages/Cropped Image.jpg", cropped_image)
 
     graygraph= cv2.cvtColor(graph, cv2.COLOR_BGR2GRAY)
     graygraphcopy = cv2.cvtColor(graphcopy,cv2.COLOR_BGR2GRAY)
     graygraph = np.float32(graygraph)
 
     dst = cv2.cornerHarris(graygraph,2,3,0.05)
-    # Look into Shi-Tomasi corner detection
+
     # Threshold for an optimal value, it may vary depending on the image.
     graygraph[dst>0.06*dst.max()]=255
 
     corneronly = graygraph-graygraphcopy
 
     corneronly[:-9,9:]=0
-    #cv2.imshow('Corneronly', corneronly)
-    #cv2.waitKey()
+
 
     (var1,var2) = np.shape(corneronly)
     indexhighlow= []
     indexlowhigh= []
     for i in range(var2-1):
-     for j in range(var1-1):
-        if corneronly[j,i] > 128:
-            if corneronly[j+1,i] < 128:
-                indexhighlow.append((j,i))
-        elif corneronly[j,i] < 128:
-            if corneronly[j+1,i] > 128:
-                indexlowhigh.append((j+1,i))
+        for j in range(var1-1):
+            if corneronly[j,i] > 128:
+                if corneronly[j+1,i] < 128:
+                    indexhighlow.append((j,i))
+            elif corneronly[j,i] < 128:
+                if corneronly[j+1,i] > 128:
+                    indexlowhigh.append((j+1,i))
 
     second_element_count = {}
 
     for tup in indexlowhigh:
-     second_element = tup[1]
-     if second_element in second_element_count:
-        second_element_count[second_element] += 1
-     else:
-        second_element_count[second_element] = 1
+        second_element = tup[1]
+        if second_element in second_element_count:
+            second_element_count[second_element] += 1
+        else:
+            second_element_count[second_element] = 1
 
     max_occurrence = max(second_element_count.values())
     max_occurrence_elements = [k for k, v in second_element_count.items() if v == max_occurrence]
     maximum = max_occurrence_elements[0]
     filtered_array = [tup for tup in indexlowhigh if tup[1] == maximum]
-    print(filtered_array)
+
 
     differences = []
     for i in range(1, len(filtered_array)):
-     difference = filtered_array[i][0] - filtered_array[i - 1][0]
-     differences.append(difference)
-    print(differences)
+        difference = filtered_array[i][0] - filtered_array[i - 1][0]
+        differences.append(difference)
+
     lengthscaley = np.ceil(np.mean(differences))
 
     for i in range(var2-1):
-     for j in range(var1-1):
-        if corneronly[j,i] > 128:
-            if corneronly[j,i+1] < 128:
-                indexhighlow.append((j,i))
-        elif corneronly[j,i] < 128:
-            if corneronly[j,i+1] > 128:
-                indexlowhigh.append((j,i+1))
+        for j in range(var1-1):
+            if corneronly[j,i] > 128:
+                if corneronly[j,i+1] < 128:
+                    indexhighlow.append((j,i))
+            elif corneronly[j,i] < 128:
+                if corneronly[j,i+1] > 128:
+                    indexlowhigh.append((j,i+1))
 
     first_element_count = {}
 
     for tup in indexlowhigh:
-     first_element = tup[0]
-     if first_element in first_element_count:
-        first_element_count[first_element] += 1
-     else:
-        first_element_count[first_element] = 1
+        first_element = tup[0]
+        if first_element in first_element_count:
+            first_element_count[first_element] += 1
+        else:
+            first_element_count[first_element] = 1
 
     max_occurrence = max(first_element_count.values())
     max_occurrence_elements = [k for k, v in first_element_count.items() if v == max_occurrence]
     maximum = max_occurrence_elements[0]
     filtered_array = [tup for tup in indexlowhigh if tup[0] == maximum]
-    print(filtered_array)
-
-    # differences = []
-    # for i in range(1, len(filtered_array)):
-    #     difference = filtered_array[i][1] - filtered_array[i - 1][1]
-    #     differences.append(difference)
-    # print(differences)
-    # for i in range(len(differences)):
-    #     if differences[i-1] < 10:
-    #         differences.pop(i-1)
-    # print(differences)
-    # lengthscale2 = np.ceil(np.mean(differences))
-    # print(lengthscale2)
 
     i = np.arange(1,len(filtered_array))
     filtered_array=np.array(filtered_array)
@@ -194,4 +161,4 @@ def lengthscalesAndEdge(load):
     diff = diff[np.where(diff>5)]
     lengthscalex = np.ceil(np.mean(diff))
     
-    return(graph,nongraph,a,b,c,d,lengthscaley,lengthscalex)
+    return [graph,nongraph,a,b,c,d,lengthscaley,lengthscalex]
--- ocr.py
+++ ocr.py
@@ -1,47 +1,52 @@
 import cv2
 import numpy as np
 import pytesseract
+from pytesseract import Output
+from lengthscalesAndEdges import lengthscalesAndEdge as le 
 
-def preprocess(imgfile, noise_removal):
-    #read the image
-    img = cv2.imread(imgfile)
+def enhance(img):
+    img = cv2.resize(img,(0,0),fx=3,fy=3)
+    gimg=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    bimg = cv2.threshold(gimg, 127, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+    cv2.imshow('enhanced image', bimg)
+    cv2.waitKey(0)    
+    return bimg
 
-    if(noise_removal == True):
-   
-        #converting to grayscale
-        gimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        #binarizing the image
-        th = 120
-        ret, bimg = cv2.threshold(gimg, th, 255, cv2.THRESH_BINARY) 
-        #using the global threshold function, can update to adaptive one.
-
-        # #noise removal
-        kernal = np.ones((1,1), np.uint8)
-        image = cv2.dilate(bimg, kernel=kernal, iterations=1)
-        image = cv2.erode(image, kernel=kernal, iterations=1)
-        image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel=kernal)
-        image = cv2.medianBlur(image, ksize=1)
-        cv2.imshow('Preprocessed Image', image)
-        cv2.waitKey(0)
-        return image
-    else:
-        #converting to grayscale
-        gimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        #binarizing the image
-        th = 120
-        ret, bimg = cv2.threshold(gimg, th, 255, cv2.THRESH_BINARY)
-        
-        cv2.imshow('Preprocessed Image', bimg)
-        cv2.waitKey(0)
-        return bimg
+def is_number(string):
+  try:
+    float(string)
+    return True  # True if string is a number contains a dot
+  except ValueError:  # String is not a number
+    return False
 
 def extractLabels(imgfile):
-    img = preprocess(imgfile, False)
-    res = pytesseract.image_to_string(img)
-    return res
+    img = cv2.imread(imgfile)
+    [graph, nongraph, a, b, c, d, lengthscaley, lengthscalex] = le(img)
+    nongraph[b-10:d+10,a-10:c+10] = 255
+    cv2.imshow('Non-graph', nongraph)
+    cv2.waitKey(0)
+    
+    #running OCR on vertical axis
+    ngv = np.copy(nongraph[:,:(a-10)])
+    ngv = enhance(ngv)
+    dic = pytesseract.image_to_data(ngv, config='--psm 4',output_type=Output.DICT)
+    n_boxes = len(dic['level'])
+    resv = []
+    for i in range(n_boxes):
+        if is_number(dic['text'][i]): 
+            resv.append(float(dic['text'][i]))
 
+    #running OCR on horizontal axis
+    ngh = np.copy(nongraph[(d+10):,:])
+    ngh = enhance(ngh)
+    dic = pytesseract.image_to_data(ngh, config='--psm 4', output_type=Output.DICT)
+    n_boxes = len(dic['level'])
+    resh = []
+    for i in range(n_boxes):
+        if is_number(dic['text'][i]): 
+            resh.append(float(dic['text'][i]))
+    
+    return [resh, resv]
 #testing 
 
-print(extractLabels("output/graph4.png"))
+print(extractLabels("testImages/test2.png"))
