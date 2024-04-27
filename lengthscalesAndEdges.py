385cbc454f39d105b0ee99790ea7004076a971d5
HeinzLoutveheBlack <prathamcsgupta@gmail.com>
2024-04-27 12:37:50 +0530

    Final Updated file for detectAxesAndLengthScales, main working function, tested for multiple images nr

--- lengthscalesAndEdges.py
+++ lengthscalesAndEdges.py
@@ -4,14 +4,12 @@ Created on Sun Mar 10 10:52:22 2024
 
 @author: Pratham Gupta
 """
-#program to function to detect the axes of the graph and
-#remove the plot area from the axes for doing OCR.
+#This is for testing of filters and basic image processing
 
 import cv2 
 import numpy as np
 
-def detectAxesAndLengthScales(load):
-
+def lengthscalesAndEdge(load):
     loadcopy = load
 
     down_width = 720
@@ -22,65 +20,78 @@ def detectAxesAndLengthScales(load):
 
     gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
     dst = cv2.Canny(gray,400,500)
-    
+
     # Threshold for an optimal value, it may vary depending on the image.
     image[dst>0.012*dst.max()]=[255,255,255]
 
     offset = cv2.cvtColor(image-imagecopy, cv2.COLOR_BGR2GRAY)
+    '''cv2.imshow('Offset',offset)
+    cv2.waitKey()'''
 
     column_sum=[]
     for i in range(down_height):
-        column_index = i
-        column_sum.append(np.sum(offset[:, column_index]))
-        i+=1
+     column_index = i
+     column_sum.append(np.sum(offset[:, column_index]))
+     i+=1
+    #print(column_sum)
+    #print(max(column_sum))
     lvline_index =column_sum.index(max(column_sum))
 
     loopline2_index=lvline_index
     while abs(loopline2_index-lvline_index)<10:
-        column_sum.pop(loopline2_index)
-        loopline2_index = column_sum.index(max(column_sum))
-        columntestline_index = loopline2_index
+     column_sum.pop(loopline2_index)
+     loopline2_index = column_sum.index(max(column_sum))
+    columntestline_index = loopline2_index
 
     if columntestline_index < lvline_index:
-        vline_index = columntestline_index
+     vline_index = columntestline_index
     else:
-        vline_index=columntestline_index+1
+     vline_index=columntestline_index+1
 
     row_sum=[]
     for i in range(down_width):
-        row_index = i
-        row_sum.append(np.sum(offset[row_index,:]))
-        i+=1
-        
+     row_index = i
+     row_sum.append(np.sum(offset[row_index,:]))
+     i+=1
+    #print(row_sum)
+    #print(max(row_sum))
     bhline_index =row_sum.index(max(row_sum))
 
     loopline_index=bhline_index
     while abs(loopline_index-bhline_index)<10:
-        row_sum.pop(loopline_index)
-        loopline_index = row_sum.index(max(row_sum))
-        rowtestline_index = loopline_index
+     row_sum.pop(loopline_index)
+     loopline_index = row_sum.index(max(row_sum))
+    rowtestline_index = loopline_index
 
     if rowtestline_index < bhline_index:
-        hline_index = rowtestline_index
+     hline_index = rowtestline_index
     else:
-        hline_index=rowtestline_index+1
+     hline_index=rowtestline_index+1
+
 
     a = lvline_index
     b = hline_index  # nz[:,0,1].min()
     c = vline_index #nz[:,0,0].max()
     d = bhline_index
 
+    #print(b,d,c,a)
+
     offset[b,a:c] =128
     offset[b:d,a] =128
     offset[b:d,c] =128
     offset[d,a:c] =128
+    # cv2.imshow('Offset', offset)
+    # cv2.waitKey()
 
     imagecopy[b,a:c] =(0,255,0)
     imagecopy[d,a:c] =(0,255,0)
     imagecopy[b:d,a] =(0,255,0)
     imagecopy[b:d,c] =(0,255,0)
 
-    graph = imagecopy[b:d,a:c]
+    # cv2.imshow('Image',imagecopy)
+    # cv2.waitKey()
+
+    graph = imagecopy[b-3:d+3,a-3:c+3]
     nongraph = imagecopy
     nongraph[b:d,a:c] = 255
     graphcopy = graph
--- tester.py
+++ tester.py
@@ -9,8 +9,7 @@ Created on Sun Mar 10 10:52:22 2024
 import cv2 
 import numpy as np
 
-def detectAxis(load):
-
+def cornerAndEdge(load):
     loadcopy = load
 
     down_width = 720
@@ -21,7 +20,7 @@ def detectAxis(load):
 
     gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
     dst = cv2.Canny(gray,400,500)
-    
+
     # Threshold for an optimal value, it may vary depending on the image.
     image[dst>0.012*dst.max()]=[255,255,255]
 
@@ -31,43 +30,43 @@ def detectAxis(load):
 
     column_sum=[]
     for i in range(down_height):
-        column_index = i
-        column_sum.append(np.sum(offset[:, column_index]))
-        i+=1
-    print(column_sum)
-    print(max(column_sum))
+     column_index = i
+     column_sum.append(np.sum(offset[:, column_index]))
+     i+=1
+    #print(column_sum)
+    #print(max(column_sum))
     lvline_index =column_sum.index(max(column_sum))
 
     loopline2_index=lvline_index
     while abs(loopline2_index-lvline_index)<10:
-        column_sum.pop(loopline2_index)
-        loopline2_index = column_sum.index(max(column_sum))
-        columntestline_index = loopline2_index
+     column_sum.pop(loopline2_index)
+     loopline2_index = column_sum.index(max(column_sum))
+    columntestline_index = loopline2_index
 
     if columntestline_index < lvline_index:
-        vline_index = columntestline_index
+     vline_index = columntestline_index
     else:
-        vline_index=columntestline_index+1
+     vline_index=columntestline_index+1
 
     row_sum=[]
     for i in range(down_width):
-        row_index = i
-        row_sum.append(np.sum(offset[row_index,:]))
-        i+=1
-    print(row_sum)
-    print(max(row_sum))
+     row_index = i
+     row_sum.append(np.sum(offset[row_index,:]))
+     i+=1
+    #print(row_sum)
+    #print(max(row_sum))
     bhline_index =row_sum.index(max(row_sum))
 
     loopline_index=bhline_index
     while abs(loopline_index-bhline_index)<10:
-        row_sum.pop(loopline_index)
-        loopline_index = row_sum.index(max(row_sum))
-        rowtestline_index = loopline_index
+     row_sum.pop(loopline_index)
+     loopline_index = row_sum.index(max(row_sum))
+    rowtestline_index = loopline_index
 
     if rowtestline_index < bhline_index:
-        hline_index = rowtestline_index
+     hline_index = rowtestline_index
     else:
-        hline_index=rowtestline_index+1
+     hline_index=rowtestline_index+1
 
 
     a = lvline_index
@@ -92,12 +91,11 @@ def detectAxis(load):
     # cv2.imshow('Image',imagecopy)
     # cv2.waitKey()
 
-    graph = imagecopy[b:d,a:c]
+    graph = imagecopy[b-3:d+3,a-3:c+3]
     nongraph = imagecopy
     nongraph[b:d,a:c] = 255
+    graphcopy = graph
 
-    '''graphcopy = imagecopy[b-3:d+3,a-3:c+3]
-    
     # Display cropped image
     # cv2.imshow("Cropped", graph)
     # cv2.waitKey()
@@ -117,12 +115,83 @@ def detectAxis(load):
     corneronly = graygraph-graygraphcopy
 
     corneronly[:-9,9:]=0
-    cv2.imshow('Corneronly', corneronly)
-    cv2.waitKey()
-    kernel = np.ones((2,2),np.float32)/4
-    dst = cv2.filter2D(corneronly,-1,kernel)
-    cv2.imshow('Corneronly',dst)
-    cv2.waitKey() '''
-    return(nongraph)
+    #cv2.imshow('Corneronly', corneronly)
+    #cv2.waitKey()
 
+    (var1,var2) = np.shape(corneronly)
+    indexhighlow= []
+    indexlowhigh= []
+    for i in range(var2-1):
+     for j in range(var1-1):
+        if corneronly[j,i] > 128:
+            if corneronly[j+1,i] < 128:
+                indexhighlow.append((j,i))
+        elif corneronly[j,i] < 128:
+            if corneronly[j+1,i] > 128:
+                indexlowhigh.append((j+1,i))
 
+    second_element_count = {}
+
+    for tup in indexlowhigh:
+     second_element = tup[1]
+     if second_element in second_element_count:
+        second_element_count[second_element] += 1
+     else:
+        second_element_count[second_element] = 1
+
+    max_occurrence = max(second_element_count.values())
+    max_occurrence_elements = [k for k, v in second_element_count.items() if v == max_occurrence]
+    maximum = max_occurrence_elements[0]
+    filtered_array = [tup for tup in indexlowhigh if tup[1] == maximum]
+    print(filtered_array)
+
+    differences = []
+    for i in range(1, len(filtered_array)):
+     difference = filtered_array[i][0] - filtered_array[i - 1][0]
+     differences.append(difference)
+    print(differences)
+    lengthscaley = np.ceil(np.mean(differences))
+
+    for i in range(var2-1):
+     for j in range(var1-1):
+        if corneronly[j,i] > 128:
+            if corneronly[j,i+1] < 128:
+                indexhighlow.append((j,i))
+        elif corneronly[j,i] < 128:
+            if corneronly[j,i+1] > 128:
+                indexlowhigh.append((j,i+1))
+
+    first_element_count = {}
+
+    for tup in indexlowhigh:
+     first_element = tup[0]
+     if first_element in first_element_count:
+        first_element_count[first_element] += 1
+     else:
+        first_element_count[first_element] = 1
+
+    max_occurrence = max(first_element_count.values())
+    max_occurrence_elements = [k for k, v in first_element_count.items() if v == max_occurrence]
+    maximum = max_occurrence_elements[0]
+    filtered_array = [tup for tup in indexlowhigh if tup[0] == maximum]
+    print(filtered_array)
+
+    # differences = []
+    # for i in range(1, len(filtered_array)):
+    #     difference = filtered_array[i][1] - filtered_array[i - 1][1]
+    #     differences.append(difference)
+    # print(differences)
+    # for i in range(len(differences)):
+    #     if differences[i-1] < 10:
+    #         differences.pop(i-1)
+    # print(differences)
+    # lengthscale2 = np.ceil(np.mean(differences))
+    # print(lengthscale2)
+
+    i = np.arange(1,len(filtered_array))
+    filtered_array=np.array(filtered_array)
+    diff = filtered_array[i]-filtered_array[i-1]
+    diff = diff[np.where(diff>5)]
+    lengthscalex = np.ceil(np.mean(diff))
+    
+    return(graph,nongraph,a,b,c,d,lengthscaley,lengthscalex)
